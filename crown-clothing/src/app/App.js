f0aee1665db617efe1805796316913c60197799f
vanshika vanshika <anandvanshika8@gmail.com>
2023-09-08 10:51:03 -0400

    created checkout page with functionaliy and total

--- crown-clothing/src/app/App.js
+++ crown-clothing/src/app/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+
 import Home from "../routes/home/Home.jsx";
 import Navigation from "../routes/navigation/Navigation.jsx";
 import Authentication from "../routes/authentication/Authentication.jsx";
 import Shop from "../routes/shop/Shop.jsx";
+import Checkout from "../routes/checkout/Checkout.jsx";
 
 const App = () => {
   return (
@@ -12,6 +14,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
--- crown-clothing/src/components/product-card/ProductCard.jsx
+++ crown-clothing/src/components/product-card/ProductCard.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/Button";
 import "./ProductCard.scss";
 
 function ProductCard({ product }) {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
       <div className="footer">
         <div className="name">{name}</div>
-        <div className="price">{price}</div>
+        <div className="price">${price}</div>
       </div>
-      <Button buttonType="invert">Add to cart</Button>
+      <Button buttonType="invert" onClick={() => addProductToCart(product)}>
+        Add to cart
+      </Button>
     </div>
   );
 }
